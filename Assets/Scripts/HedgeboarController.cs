e12c5301b9507b4dea752d4a10f065acd4f0bcd5
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-04-10 10:37:48 +0200

    updt

--- Assets/Scripts/HedgeboarController.cs
+++ Assets/Scripts/HedgeboarController.cs
@@ -2,44 +2,80 @@ using UnityEngine;
 
 public class HedgeboarController : MonoBehaviour
 {
+    [SerializeField] private float speed = 7.0f;
+
     private Animator animator;
     private Rigidbody2D rigidbody2d;
 
     private bool facingRight;
-    private bool canCharge;
+    private bool canFlip;
+    private bool isCharging;
+
     private float nextFlip;
     private float flipTimer;
+    private float startChargeTime;
+    private float chargeDelay;
 
     private void Start()
-    {
+    {   
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         facingRight = false;
-        canCharge = true;
+        canFlip = true;
+        isCharging = false;
         nextFlip = 0f;
         flipTimer = 2.5f;
+        chargeDelay = .35f;
+        startChargeTime = 0f;
+        
     }
 
     private void Update()
     {
         if (Time.time > nextFlip)
         {
-            if (Random.Range(0, 10) > 4) Flip();
+            if (canFlip && Random.Range(0, 10) > 4) Flip();
             nextFlip = Time.time + flipTimer;
+        }
+        animator.SetBool("isCharging", isCharging);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (facingRight != other.transform.position.x > transform.position.x) Flip();
+            canFlip = false;
+            isCharging = true;
+            startChargeTime = Time.time + chargeDelay;
         }
     }
 
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Time.time > startChargeTime)
+        {
+            rigidbody2d.AddForce(new Vector2(x: facingRight ? +1 : -1, y: 0) * speed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canFlip = true;
+            isCharging = false;
+            rigidbody2d.velocity = Vector2.zero;
+        }
+    }
 
     private void Flip()
     {
-        if (!canCharge) return;
-
         transform.localScale = new(
             transform.localScale.x * -1,
             transform.localScale.y,
             transform.localScale.z);
-
         facingRight = !facingRight;
     }
 }
